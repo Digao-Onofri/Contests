79810e5176c17216338def9f8a4a1c067dcfa430
Digao-Onofri <your_email@example.com>
2026-02-04 11:40:59 -0300

    [fix] including new solution and fixes

--- Codeforces/Groups/Maratona Mineira/IX/j.cpp
+++ Codeforces/Groups/Maratona Mineira/IX/j.cpp
@@ -1,13 +1,14 @@
 /*
     Name: Um dia no museu
     Link: https://codeforces.com/group/YgJmumGtHD/contest/103794/problem/J
-    criar uma reta a partir dos pontos?
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct{int x, y;}Ponto;
+class Ponto{
+    public: int x, y;
+};
 
 //Função para calcular o produto vetorial
 double crossProduct(Ponto a, Ponto b){
@@ -15,30 +16,42 @@ double crossProduct(Ponto a, Ponto b){
 }
 
 //Ponto p1 é uma aresta e p2 tambem
+//Return true when the intersect happen
 bool doIntersect(Ponto p1, Ponto p2, Ponto p3, Ponto p4){
     double d1 = crossProduct({p3.x - p1.x, p3.y - p1.y}, {p2.x - p1.x, p2.y - p1.y});
     double d2 = crossProduct({p4.x - p1.x, p4.y - p1.y}, {p2.x - p1.x, p2.y - p1.y});
     double d3 = crossProduct({p1.x - p3.x, p1.y - p3.y}, {p4.x - p3.x, p4.y - p3.y});
     double d4 = crossProduct({p2.x - p3.x, p2.y - p3.y}, {p4.x - p3.x, p4.y - p3.y});
 
-    return (d1 * d2 < 0) && (d3 * d4 < 0);
+    return (d1 * d2 <= 0) && (d3 * d4 <= 0);
 }
 
 int main(){
     int dots;
-    int x, y;
+    int x, y, i;
     int posx, posy;
     vector<Ponto> all;
 
     cin >> dots;
-    while(dots--){
+    for(i = 0; i < dots; i++){
         cin >> x >> y;
         all.push_back({x, y});
     }
 
     cin >> posx >> posy;
 
+    for(i = 0; i < dots; i++){
+        if(doIntersect(all[i], all[(i + 1) % dots], Ponto{posx, posy}, all[(i - 1 + dots) % dots])){
+            cout << "N" << endl;
+            return 0;
+        }
+        if(doIntersect(all[i], all[(i + 1) % dots], Ponto{posx, posy}, all[(i + 2) % dots])){
+            cout << "N" << endl;
+            return 0;
+        }  
+    }
 
+    cout << "S" << endl;
 
     return 0;
 }

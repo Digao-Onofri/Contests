28f239b832ef67719776d5b571393fc55d88cf1a
Digao-Onofri <goncalves.onofri@gmail.com>
2026-02-16 15:04:47 -0300

    [add] including new contests to solve

---
+++ Codeforces/Contests/Div. 1/1079/*e1.cpp
@@ -0,0 +1,4 @@
+/*
+    Name: Interactive Graph (Simple Version)
+    Link: https://codeforces.com/contest/2197/problem/E1
+*/
---
+++ Codeforces/Contests/Div. 2/1077/*a.cpp
@@ -0,0 +1,4 @@
+/*
+    Name: Divisible Permutation
+    Link: https://codeforces.com/contest/2188/problem/A
+*/
